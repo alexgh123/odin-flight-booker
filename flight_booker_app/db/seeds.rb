fd0895e3e60fa91dfef8599e72f6c56cd54e3891
Alex Hardt <hardt.alex@gmail.com>
2015-04-30 12:43:51 -0500

    updated seed databse and updated view for new databse attributes

--- flight_booker_app/db/seeds.rb
+++ flight_booker_app/db/seeds.rb
@@ -7,26 +7,47 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#<Flight id: nil, created_at: nil, updated_at: nil, origin_code: nil, destination_code: nil>
+#<Flight id: nil, created_at: nil, updated_at: nil, origin_code: nil, destination_code: nil, hours: nil, minutes: nil, departure_date_time: nil>
 
 
 Flight.create!(origin_code: "DFW",
-               destination_code: "NYC")
+               destination_code: "NYC",
+               hours: 2,
+               minutes: 2,
+               departure_date_time: Time.zone.now)
 
 Flight.create!(origin_code: "SFO",
-               destination_code: "NYC")
+               destination_code: "NYC",
+               hours: 2,
+               minutes: 2,
+               departure_date_time: Time.zone.now)
 
 Flight.create!(origin_code: "DFW",
-               destination_code: "NYC")
+               destination_code: "NYC",
+               hours: 3,
+               minutes: 3,
+               departure_date_time: Time.zone.now)
 
 Flight.create!(origin_code: "SFO",
-               destination_code: "NYC")
+               destination_code: "NYC",
+               hours: 9,
+               minutes: 22,
+               departure_date_time: Time.zone.now)
 
 Flight.create!(origin_code: "DFW",
-               destination_code: "NYC")
+               destination_code: "NYC",
+               hours: 1,
+               minutes: 9,
+               departure_date_time: Time.zone.now)
 
 Flight.create!(origin_code: "SFO",
-               destination_code: "MDW")
+               destination_code: "MDW",
+               hours: 8,
+               minutes: 7,
+               departure_date_time: Time.zone.now)
 
 Flight.create!(origin_code: "ORD",
-               destination_code: "NYC")
+               destination_code: "NYC",
+               hours: 2,
+               minutes: 5,
+               departure_date_time: Time.zone.now)
