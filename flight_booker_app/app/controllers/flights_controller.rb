029f3bff3e67365c0dd6ca7323968f5140ecc58a
Alex Hardt <hardt.alex@gmail.com>
2015-05-01 10:49:33 -0500

    this works, and does what I want it to, but i think I should be able to querrry the databse in a better way

--- flight_booker_app/app/controllers/flights_controller.rb
+++ flight_booker_app/app/controllers/flights_controller.rb
@@ -1,5 +1,11 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @airports = Airport.all
+
+    # so in the index item, i need to make airport codes available via the airport id
+
   end
+
+
 end
