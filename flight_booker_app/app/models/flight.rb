d7dd8a47577e3c0cc71969334dd542fd020a3b8c
Alex Hardt <hardt.alex@gmail.com>
2015-05-01 09:53:28 -0500

    flight and airport model connected, i just had to make one more update to model, should be good now

--- flight_booker_app/app/models/flight.rb
+++ flight_booker_app/app/models/flight.rb
@@ -1,4 +1,5 @@
 class Flight < ActiveRecord::Base
   has_one :airport, foreign_key: "origin_airport_id"
-  # has_one :airport, foreign_key: "desination_airport_id"
+  has_one :airport, foreign_key: "desination_airport_id"
+  # destination and origin: !== CANT BE EQUAL
 end
