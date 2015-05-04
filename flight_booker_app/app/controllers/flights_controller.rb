7d311b5ac14acdfa2161764cd036fc0e38094176
Alex Hardt <hardt.alex@gmail.com>
2015-05-04 15:43:39 -0500

    drop down menu works and passes args to url, but now need to show flight results

--- flight_booker_app/app/controllers/flights_controller.rb
+++ flight_booker_app/app/controllers/flights_controller.rb
@@ -1,8 +1,15 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.search(params[:search])
+    @flights = Flight.all
     @airports = Airport.all
 
+  end
+
+   def index
+    @flights = Flight.search(params)
+    @airport_codes = Airport.all.collect { |a|  [a.airport_code, a.id] }
+    @num_passengers = (1..4).to_a.collect { |x| [x, x] }
+    # @dates = Flight.flight_dates
   end
 
 
--- flight_booker_app/app/models/flight.rb
+++ flight_booker_app/app/models/flight.rb
@@ -4,7 +4,17 @@ class Flight < ActiveRecord::Base
   # destination and origin: !== CANT BE EQUAL
 
 
+  def self.search(params)
+    if params[:search]
+      Flight.where(origin_airport_id: params[:from], destination_airport_id: params[:to])
+            .includes(:from_airport, :to_airport)
+    end
+  end
 
+   def self.on_date(date)
+    # http://stackoverflow.com/questions/2381718/rails-activerecord-date-between
+    where(departure_date_time: date.beginning_of_day..date.end_of_day)
+  end
 
 
 end
