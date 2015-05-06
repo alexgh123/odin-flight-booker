3ef88bbd1ebf9fac234beab603751828121512c5
Alex Hardt <hardt.alex@gmail.com>
2015-05-06 10:44:40 -0500

    passenger name added to passenger data model

--- flight_booker_app/db/schema.rb
+++ flight_booker_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506153906) do
+ActiveRecord::Schema.define(version: 20150506154316) do
 
   create_table "airports", force: :cascade do |t|
     t.datetime "created_at",   null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150506153906) do
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
