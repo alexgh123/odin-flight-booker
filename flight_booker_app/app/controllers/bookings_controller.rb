d061750ab9c0d15276863b880df1ea211b12cde0
Alex Hardt <hardt.alex@gmail.com>
2015-05-06 12:07:51 -0500

    building new booking form

--- flight_booker_app/app/controllers/bookings_controller.rb
+++ flight_booker_app/app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def index
+    @bookings = Booking.all
+    @flight = Flight.find(params[:id])
+  end
+
+
 end
