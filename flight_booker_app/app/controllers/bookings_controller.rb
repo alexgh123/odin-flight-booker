4440fb8357d4fe5b51bc79edf9ffa4ac677f5b7e
Alex Hardt <hardt.alex@gmail.com>
2015-05-07 09:10:15 -0500

    making booking page look better, actually using strftime method on datetime, very helpful

--- flight_booker_app/app/controllers/bookings_controller.rb
+++ flight_booker_app/app/controllers/bookings_controller.rb
@@ -3,6 +3,8 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
+    @origin_airport =  Airport.find(@flight.origin_airport_id).airport_name
+    @destination_airport = Airport.find(@flight.destination_airport_id).airport_name
   end
 
   def index
