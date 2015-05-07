eeb7def8eaf19751d82795365977db70f6d0ba7f
Alex Hardt <hardt.alex@gmail.com>
2015-05-07 10:01:54 -0500

    can create new bookings, have a show for it, need to create passenger add fields now too

--- flight_booker_app/app/controllers/bookings_controller.rb
+++ flight_booker_app/app/controllers/bookings_controller.rb
@@ -11,5 +11,26 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+   def create
+    @booking = Booking.create(booking_params)
+    # @event = Event.find(params[:event_id])
+    if @booking.save
+      flash[:success] = "booking created!"
+      redirect_to @booking
+    else
+      flash[:failure] = "you are an idiot AND didn't create a booking"
+      redirect_to new_booking_path
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id)
+    end
+
 
 end
