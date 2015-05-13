01585d1bb26d8073afb7d15c9c6e5c22bec5478b
Alex Hardt <hardt.alex@gmail.com>
2015-05-13 12:13:02 -0500

    just api playing around thigns

--- flight_booker_app/app/controllers/flights_controller.rb
+++ flight_booker_app/app/controllers/flights_controller.rb
@@ -9,6 +9,7 @@ class FlightsController < ApplicationController
     @airport_codes = Airport.all.collect { |a|  [a.airport_code, a.id] }
     @num_passengers = (1..4).to_a.collect { |x| [x, x] }
     @dates = Flight.flight_date_time
+    # render :nothing => true, :status => 404 #just gives me a blank page
 
   end
 
