9378d3511229500acbdeb81c81ba53adb4157e66
Alex Hardt <hardt.alex@gmail.com>
2015-06-01 18:19:44 -0500

    starting to make passenger mailer from ruby on rails guides 2.2.2

--- flight_booker_app/app/mailers/passenger_mailer.rb
+++ flight_booker_app/app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@flightbooker.com'
+
+  def flight_confirm_email(passenger)
+    @passenger = passenger
+    @url = 'http://example.com/login'
+    mail(to: @passenger.email, subject: "Your recently booked flight confirmation")
+  end
+
 end
