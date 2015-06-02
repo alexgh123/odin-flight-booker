5a91636e2261266a91c0bfccd7d89046dab59776
Alex Hardt <hardt.alex@gmail.com>
2015-06-02 09:57:39 -0500

    add flight confirmation email in html and text versions

--- flight_booker_app/app/mailers/passenger_mailer.rb
+++ flight_booker_app/app/mailers/passenger_mailer.rb
@@ -1,7 +1,7 @@
 class PassengerMailer < ApplicationMailer
   default from: 'notifications@flightbooker.com'
 
-  def flight_confirm_email(passenger)
+  def flight_confirmation_email(passenger)
     @passenger = passenger
     @url = 'http://example.com/login'
     mail(to: @passenger.email, subject: "Your recently booked flight confirmation")
